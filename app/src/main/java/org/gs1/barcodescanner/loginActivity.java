96bc40dbae998d35065c8666b0a18186992b143a
daniel.rodriguez <daniel.rodriguez@gs1.org>
2019-05-30 13:17:05 -0400

    Added check box to save username and password

--- app/src/main/java/org/gs1/barcodescanner/loginActivity.java
+++ app/src/main/java/org/gs1/barcodescanner/loginActivity.java
@@ -1,10 +1,13 @@
 package org.gs1.barcodescanner;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -26,19 +29,33 @@ public class loginActivity extends AppCompatActivity {
     public EditText uname;
     public EditText pass;
 
+    private CheckBox logCheck;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String TEXT = "text";
+    public static final String TEXT2 = "text2";
+    public static final String SWITCH1 = "switch1";
+
+    private String text;
+    private String text2;
+    private boolean switchOnOff;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        logCheck = (CheckBox)findViewById(R.id.logCheck);
+        uname = (EditText)findViewById(R.id.username);
+        pass = (EditText)findViewById(R.id.password);
+        res = (TextView)findViewById(R.id.result);
         Button btn = (Button)findViewById(R.id.login);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uname = (EditText)findViewById(R.id.username);
-                pass = (EditText)findViewById(R.id.password);
-                res = (TextView)findViewById(R.id.result);
+
+
 
                 OkHttpClient client = new OkHttpClient();
                 MediaType JSON = MediaType.parse("application/json charset=utf-8");
@@ -81,5 +98,58 @@ public class loginActivity extends AppCompatActivity {
                 });
             }
         });
+//////////////////////////////////////////////////////////////////////////////////////////////////
+      logCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (logCheck.isChecked())
+                    saveData();
+                else
+                    clearData();
+            }
+        });
+
+        loadData();
+        updateView();
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
     }
+//////////////////////////////////////////////////////////////////////////////////////////////////
+   private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(TEXT, uname.getText().toString());
+       editor.putString(TEXT2, pass.getText().toString());
+        editor.putBoolean(SWITCH1, logCheck.isChecked());
+
+        editor.apply();
+
+        //Toast.makeText(this,"Data saved", Toast.LENGTH_SHORT).show();
+    }
+
+    private  void clearData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(TEXT, "");
+        editor.putString(TEXT2, "");
+        editor.putBoolean(SWITCH1, false);
+
+        editor.apply();
+    }
+
+    public void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT, "");
+        text2 = sharedPreferences.getString(TEXT2, "");
+        switchOnOff = sharedPreferences.getBoolean(SWITCH1,false);
+    }
+
+    public void updateView(){
+        uname.setText(text);
+        pass.setText(text2);
+        logCheck.setChecked(switchOnOff);
+    }
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
 }
